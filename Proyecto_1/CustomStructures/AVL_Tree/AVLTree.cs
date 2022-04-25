0f037573d00643b6ae29cc6d64a84ba1b7b99d58
pcpisquiy <pcpisquiy@gmail.com>
2022-04-24 22:44:53 -0600

    arbol avl es funcional paraguardar datos

---
+++ Proyecto_1/CustomStructures/AVL_Tree/AVLTree.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace CustomStructures.AVL_Tree
+{
+    public class AVLTree<T>:Itree_AVL<T>
+    {
+        Func<T, T, int> CompareTo;
+        Node<T> RootNode;
+        public int Count;
+        public AVLTree(Func<T, T, int> CompareTo)
+        {
+            this.CompareTo = CompareTo;
+            RootNode = null;
+            Count = 0;
+        }
+        public Node<T> FindParent(T value)
+        {
+            Node<T> nodoAnterior = null;
+            Node<T> nodoActual = RootNode;
+
+            while (nodoActual != null)
+            {
+                if (nodoActual.Value.Equals(value))
+                {
+                    return nodoAnterior;
+                }
+                else if (CompareTo(value,nodoActual.Value) > 0)
+                {
+                    nodoAnterior = nodoActual;
+                    nodoActual = nodoActual.derecha;
+                }
+                else
+                {
+                    nodoAnterior = nodoActual;
+                    nodoActual = nodoActual.izquierda;
+                }
+            }
+
+            return null;
+        }
+        public Node<T> Find(T value)
+        {
+            Node<T> nodoActual = RootNode;
+
+            while (nodoActual != null)
+            {
+                if (nodoActual.Value.Equals(value))
+                {
+                    return nodoActual;
+                }
+                else if (CompareTo(nodoActual.Value,value) < 0)
+                {
+                    nodoActual = nodoActual.derecha;
+                }
+                else
+                {
+                    nodoActual = nodoActual.izquierda;
+                }
+            }
+            return null;
+        }
+        private Node<T> FindParent(Node<T> objetivo)
+        {
+            Node<T> nodoAnterior = null;
+            Node<T> nodoActual = RootNode;
+
+            while (nodoActual != null)
+            {
+                if (nodoActual.Equals(objetivo))
+                {
+                    return nodoAnterior;
+                }
+                else if (CompareTo(objetivo.Value,nodoActual.Value) > 0)
+                {
+                    nodoAnterior = nodoActual;
+                    nodoActual = nodoActual.derecha;
+                }
+                else
+                {
+                    nodoAnterior = nodoActual;
+                    nodoActual = nodoActual.izquierda;
+                }
+            }
+
+            return null;
+        }
+        public void Add(T Value)
+        {
+            if (RootNode == null)
+            {
+                RootNode = new Node<T>();
+                RootNode.Value = Value;
+                Count++;
+                return;
+            }
+            Count++;
+            Add(Value, RootNode);
+        }
+        private void Add(T Value, Node<T> iterando)
+        {
+            bool shouldAdd = false;
+            if (CompareTo(Value,iterando.Value) < 0)
+            {
+                if (iterando.izquierda != null)
+                {
+                    Add(Value, iterando.izquierda);
+                }
+                else
+                {
+                    shouldAdd = true;
+                }
+            }
+            else if((CompareTo(Value,iterando.Value) > 0))
+            {
+                if (iterando.derecha != null)
+                {
+                    Add(Value, iterando.derecha);
+                }
+                else
+                {
+                    shouldAdd = true;
+                }
+            }
+
+            if (shouldAdd)
+            {
+                if (
+                    CompareTo(Value,iterando.Value) < 0)
+                {
+                    if (iterando.izquierda == null) { iterando.izquierda = new Node<T>(); }
+                    iterando.izquierda.Value = Value;
+                }
+                else  if ((CompareTo(Value,iterando.Value) > 0))
+                {
+                    if (iterando.derecha == null) { iterando.derecha = new Node<T>(); }
+                    iterando.derecha.Value = Value;
+                }
+                shouldAdd = false;
+            }
+
+            while (Math.Abs(iterando.Balance()) > 1)
+            {
+                if (iterando.Balance() > 1)
+                {
+                    LeftRotation(iterando);
+                }
+                else if (iterando.Balance() < 1)
+                {
+                    RightRotation(iterando);
+                }
+            }
+        }
+        private void LeftRotation(Node<T> targetNode)
+        {
+            Node<T> parentNode = FindParent(targetNode);
+            Node<T> newHead = targetNode.derecha;
+            Node<T> tempHolder;
+
+            if (newHead.derecha == null && newHead.izquierda != null)
+            {
+                RightRotation(newHead);
+                newHead = targetNode.derecha;
+            }
+
+            if (parentNode != null)
+            {
+                if (parentNode.derecha == targetNode)
+                {
+                    parentNode.derecha = newHead;
+                }
+                else
+                {
+                    parentNode.izquierda = newHead;
+                }
+            }
+            else
+            {
+                RootNode = newHead;
+            }
+            tempHolder = newHead.izquierda;
+
+            newHead.izquierda = targetNode;
+            targetNode.derecha = tempHolder;
+        }
+        private void RightRotation(Node<T> targetNode)
+        {
+            Node<T> parentNode = FindParent(targetNode);
+            Node<T> newHead = targetNode.izquierda;
+            Node<T> tempHolder;
+
+            if (newHead.izquierda == null && newHead.derecha != null)
+            {
+                LeftRotation(newHead);
+                newHead = targetNode.izquierda;
+            }
+
+            if (parentNode != null)
+            {
+                if (parentNode.izquierda == targetNode)
+                {
+                    parentNode.izquierda = newHead;
+                }
+                else
+                {
+                    parentNode.derecha = newHead;
+                }
+            }
+            else
+            {
+                RootNode = newHead;
+            }
+            tempHolder = newHead.derecha;
+
+            newHead.derecha = targetNode;
+            targetNode.izquierda = tempHolder;
+        }
+        public bool Remove(T targetValue)
+        {
+            if (RootNode == null)
+            {
+                return false;
+            }
+
+            Node<T> targetNode = Find(targetValue);
+            if (targetNode == null)
+            {
+                return false;
+            }
+            Node<T> nodoActual = RootNode;
+
+            Count--;
+            Delete(targetNode, nodoActual);
+            return true;
+        }
+        private void Delete(Node<T> targetNode, Node<T> nodoActual)
+        {
+            if (targetNode == nodoActual)
+            {
+                Node<T> LeftMax = targetNode.FindReplacement();
+                Node<T> parentNode = FindParent(targetNode);
+
+                if (LeftMax != null)
+                {
+                    Node<T> replacementNode = new Node<T>();
+                    replacementNode.Value = LeftMax.Value;
+                    replacementNode.izquierda = targetNode.izquierda;
+                    replacementNode.derecha = targetNode.derecha;
+
+                    if (targetNode != RootNode)
+                    {
+                    //    if (targetNode.IsLessThan(parentNode))
+                    //    {
+                    //        parentNode.LeftChild = replacementNode;
+                    //    }
+                    //    else
+                    //    {
+                    //        parentNode.RightChild = replacementNode;
+                    //    }
+                    }
+                    else
+                    {
+                        RootNode = replacementNode;
+                    }
+                    nodoActual = replacementNode;
+                }
+                else
+                {
+                    if (targetNode != RootNode)
+                    {
+                    //    if (targetNode.IsLessThan(parentNode) || targetNode.Value.Equals(parentNode.Value))
+                    //    {
+                    //        parentNode.LeftChild = targetNode.RightChild;
+                    //    }
+                    //    else
+                    //    {
+                    //        parentNode.RightChild = targetNode.RightChild;
+                    //    }
+                    }
+                    else
+                    {
+                        RootNode = targetNode.derecha;
+                    }
+                }
+
+                if (LeftMax != null)
+                {
+                    Delete(LeftMax, nodoActual.izquierda);
+                }
+            }
+            //else if (nodoActual.IsLessThan(targetNode))
+            //{
+            //    Delete(targetNode, nodoActual.RightChild);
+            //}
+            else
+            {
+                Delete(targetNode, nodoActual.izquierda);
+            }
+
+            while (Math.Abs(nodoActual.Balance()) > 1)
+            {
+                if (nodoActual.Balance() > 1)
+                {
+                    LeftRotation(nodoActual);
+                }
+                else
+                {
+                    RightRotation(nodoActual);
+                }
+            }
+        }
+        public List<T> InOrder()
+        {
+            List<T> returnList = new List<T>();
+            InOrder(RootNode);
+
+            return returnList;
+
+            void InOrder(Node<T> startingNode)
+            {
+                if (startingNode.izquierda != null)
+                {
+                    InOrder(startingNode.izquierda);
+                }
+                returnList.Add(startingNode.Value);
+                if (startingNode.derecha != null)
+                {
+                    InOrder(startingNode.derecha);
+                }
+            }
+        }
+
+    }
+}
---
+++ Proyecto_1/CustomStructures/Binary_Tree/BinaryTree.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CustomStructures.BinaryTree
+{
+   public class BinaryTree<T>:Itree<T>
+    {
+        Func<T, T, int> Comparer;
+        private int contador=0;
+        private int peso = 0;
+        private Node<T> Raiz = null;
+
+        public BinaryTree(Func<T, T, int> comparer) { this.Comparer = comparer; }
+        public int Altura() {
+            int cont1 = 0;
+            int cont2 = 0;
+            Node<T> Trabajo = new Node<T>();
+            Trabajo = Raiz;
+
+            while (Trabajo.Izquierda != null) {
+                Trabajo = Trabajo.Izquierda;
+                cont1++;
+            }
+            Trabajo = Raiz;
+            while (Trabajo.Derecha != null)
+            {
+                Trabajo = Trabajo.Derecha;
+                cont2++;
+            }
+
+            if (cont1 >= cont2)
+            {
+                return cont1;
+            }
+            else {
+                return cont2;
+            }
+
+        }
+        public void Add(T value)
+        {
+            Insert(value,  null);
+            contador = 0;
+        }
+        private T Insert(T Value, Node<T> Iterando)
+        {
+            if (Raiz == null)
+            {
+                Raiz = new Node<T>();
+                Raiz.Value = Value;
+                peso++;
+                return Raiz.Value;
+            }
+            else
+            {
+                if (Iterando == null)
+                {
+                    Iterando = new Node<T>();
+                    if (contador == 0)
+                    {
+                        Iterando = Raiz;
+                        contador++;
+                    }
+                }
+            }
+            int result = Comparer( Value, Iterando.Value);
+            if (result < 0)
+            {
+                if (Iterando.Izquierda == null)
+                {
+                    Iterando.Izquierda = new Node<T>();
+                    Iterando.Izquierda.Value = Value;
+                    peso++;
+                    return Value;
+                }
+                else {
+                    return Insert(Value, Iterando.Izquierda);
+                }
+            }
+            else if (result > 0)
+            {
+                if (Iterando.Derecha == null)
+                {
+                    Iterando.Derecha = new Node<T>();
+                    Iterando.Derecha.Value = Value;
+                    peso++;
+                    return Value;
+                }
+                else
+                {
+                    return Insert(Value, Iterando.Derecha);
+                }
+            }
+                return Iterando.Value;
+           
+
+        }
+        protected T GetMinimun()
+        {
+            Node<T> trabajo = Raiz;
+            while (trabajo.Izquierda != null)
+            {
+                trabajo = trabajo.Izquierda;
+            }
+            return trabajo.Value;
+
+        }
+        protected Node<T> GetMinimun(string tipo)
+        {
+            Node<T> trabajo = Raiz;
+            while (trabajo.Izquierda != null)
+            {
+                trabajo = trabajo.Izquierda;
+            }
+            return trabajo;
+
+        }
+        protected T GetMaximun()
+        {
+            Node<T> trabajo = Raiz;
+            while (trabajo.Derecha != null)
+            {
+                trabajo = trabajo.Derecha;
+            }
+            return trabajo.Value;
+
+        }
+
+        protected Node<T> GetMaximun(string tipo)
+        {
+            Node<T> trabajo = Raiz;
+            while (trabajo.Derecha != null)
+            {
+                trabajo = trabajo.Derecha;
+            }
+            return trabajo;
+
+        }
+        public void Remove(T Value)
+        {
+            internalDelete(Value, Raiz);
+        }
+        private T internalDelete(T Value, Node<T> Iterando)
+        {
+            if (Iterando == null)
+            {
+                Iterando = new Node<T>();
+                return Iterando.Value;
+            }
+            else {
+                int result = Comparer(Value, Iterando.Value);
+                if (result < 0)
+                {
+                    Iterando.Izquierda.Value = internalDelete(Value, Iterando.Izquierda);
+                }
+                else if (result > 0)
+                {
+                    Iterando.Derecha.Value = internalDelete(Value, Iterando.Derecha);
+                }
+                else {
+                    if (Iterando.Izquierda == null && Iterando.Derecha == null)
+                    {
+                        Iterando = null;
+                        return default;
+                    }
+                    else if (Iterando.Izquierda == null)
+                    {
+                        Node<T> Padre = BuscarPadre(Iterando.Derecha.Value, Raiz);
+                        Padre.Derecha = Iterando.Derecha;
+                        return Iterando.Value;
+                    }
+                    else if (Iterando.Derecha == null)
+                    {
+                        Node<T> Padre = BuscarPadre(Iterando.Izquierda.Value, Raiz);
+                        Padre.Izquierda = Iterando.Izquierda;
+                        return Iterando.Value;
+                    }
+                    else {
+                        Node<T> min = GetMinimun("Node");
+                        Iterando.Value = min.Value;
+                        Iterando.Derecha.Value = internalDelete(min.Value, Iterando.Derecha);
+                    }
+                }
+                return Iterando.Value;
+            }
+
+        }
+        private Node<T> BuscarPadre(T value, Node<T> node) {
+            if (node == null) {
+                return null;
+            }
+            if (node.Izquierda != null) {
+                if (node.Izquierda.Value.Equals(value)) {
+                    return node;
+                }
+            }
+            if (node.Derecha != null)
+            {
+                if (node.Derecha.Value.Equals(value))
+                {
+                    return node;
+                }
+            }
+            int result = Comparer(node.Value, value);
+            if (node.Izquierda!=null&&result < 0)
+            {
+                node.Izquierda.Value = internalDelete(value, node.Izquierda);
+            }
+            else if (node.Derecha!=null&&result > 0)
+            {
+                node.Derecha.Value = internalDelete(value, node.Derecha);
+            }
+            return node;
+        }
+
+        List<T> ArbolOrdenado = new List<T>();
+        public T Search(T Value, Func<T, T, bool> Filtro)
+        {
+            ///Lista enviada
+        
+            foreach (var item in ArbolOrdenado) {
+                if (Filtro(Value, item)) {
+                    return item;
+                 }
+            }
+            return default;
+        }
+        List<T> Data = new List<T>();
+        public List<T> inorder() {
+            return InternalInorder(Raiz);
+        }
+
+        private List<T> InternalInorder(Node<T> Node)
+        {
+            if (Node != null) {
+                InternalInorder(Node.Izquierda);
+                Data.Add(Node.Value);
+                InternalInorder(Node.Derecha);
+            }
+            return Data;
+        }
+    }
+}
